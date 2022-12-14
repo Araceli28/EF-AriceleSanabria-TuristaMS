f614aa4832eb1070dd301c2faa5d8effcc2e8f8e
Araceli28 <98657580+Araceli28@users.noreply.github.com>
2022-12-13 22:50:45 -0600

    Add files via upload

---
+++ turistaScript.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO hoteldb.turista VALUES (1,'Av La Marina 4655','Aricele Sanabria','984781712');
+INSERT INTO hoteldb.turista VALUES (2,'Av Brasil 1245','Isaac Valentino','970804522');
+SELECT * FROM hoteldb.turista;
